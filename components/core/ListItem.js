cc07e3a611b063a0417765818603e2dc7d8b32ea
sidekick9497 <iharish@protonmail.com>
2021-03-05 01:47:48 +0530

    added baseListView to home screen for design purpose.

--- components/core/ListItem.js
+++ components/core/ListItem.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {View, Text} from 'react-native';
+import {View, Text, StyleSheet} from 'react-native';
 
 const ListItem = ({ title }) => (
     <View style={styles.item}>
@@ -7,4 +7,20 @@ const ListItem = ({ title }) => (
     </View>
   );
 
-export default Item  
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    marginTop: 2,
+  },
+  item: {
+    backgroundColor: '#f9c2ff',
+    padding: 20,
+    marginVertical: 8,
+    marginHorizontal: 16,
+  },
+  title: {
+    fontSize: 32,
+  },
+});
+
+export default ListItem  
--- components/screens/HomeScreen.js
+++ components/screens/HomeScreen.js
@@ -3,11 +3,12 @@ import React from 'react';
 import { StyleSheet, Text, View,Button } from 'react-native';
 import Search from '../core/Search';
 import {Appearance} from 'react-native-appearance'
-
+import BaseListView from '../core/BaseListView'
 const HomeScreen = (props) => {
   return (
     <View style={styles.container}>
       <Search searchText = {props.searchText} searchTextEvent = {props.searchTextEvent} />
+      <BaseListView/>
       <Text style={styles.center}>
         sys theme : {Appearance.getColorScheme()}
       </Text>
