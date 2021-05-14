908fd96bc81f2fcd95351490cb47a70e3ebf542a
sidekick9497 <iharish@protonmail.com>
2021-05-15 00:22:33 +0530

    list item styles and homescreen changes

--- components/screens/HomeScreen.js
+++ components/screens/HomeScreen.js
@@ -1,14 +1,20 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View,Button } from 'react-native';
+import { StyleSheet, View,Button, ScrollView } from 'react-native';
 import Search from '../core/Search';
 import {Appearance} from 'react-native-appearance'
 import BaseListView from '../core/BaseListView'
+import { Text } from 'react-native-elements';
 const HomeScreen = (props) => {
   return (
     <View style={styles.container}>
+      <ScrollView>
       <Search searchText = {props.searchText} searchTextEvent = {props.searchTextEvent} />
+      <Text h1>Recent</Text>
       <BaseListView/>
+       <Text h1>Suggestions</Text>
+      <BaseListView/>
+      </ScrollView>
       <Text style={styles.center}>
         sys theme : {Appearance.getColorScheme()}
       </Text>
